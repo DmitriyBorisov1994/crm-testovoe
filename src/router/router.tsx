d0d509d98eee24e4ed306b199d73d3d268d13a36
Dmitriy Borisov <borisovdv1994@ya.ru>
2023-04-23 19:51:05 +0400

    замена на HashRouter для gh-pages

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,9 +1,9 @@
-import { Route, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
+import { Route, createBrowserRouter, createHashRouter, createRoutesFromElements } from "react-router-dom";
 import RequireAuth from "../hoc/RequireAuth";
 import ContactsPage from "../pages/ContactsPage";
 import LoginPage from "../pages/LoginPage";
 
-export const router = createBrowserRouter(createRoutesFromElements(
+export const router = createHashRouter(createRoutesFromElements(
    <>
       <Route path='/' element={<RequireAuth><ContactsPage /></RequireAuth>} />
       <Route path="/login" element={<LoginPage />} />
